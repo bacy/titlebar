5ab358feae83eaad1b53ba72e9074505185421d3
chenbo01 <chenbo01@wondersgroup.com>
2015-10-16 11:27:41 +0800

    code submit

---
+++ sample/src/main/java/titlebar/bacy/com/sample/MainActivity.java
@@ -0,0 +1,94 @@
+package titlebar.bacy.com.sample;
+
+import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bacy.view.titlebar.TitleBar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView mCollectView;
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (hasKitKat() && !hasLollipop()) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        } else if (hasLollipop()) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        final TitleBar titleBar = (TitleBar) findViewById(R.id.title_bar);
+
+        titleBar.setImmersive(true);
+
+        titleBar.setBackgroundColor(Color.parseColor("#64b4ff"));
+
+        titleBar.setLeftImageResource(R.mipmap.back_green);
+        titleBar.setLeftText("返回");
+        titleBar.setLeftTextColor(Color.WHITE);
+        titleBar.setLeftClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        titleBar.setTitle("文章详情");
+        titleBar.setTitleColor(Color.WHITE);
+        titleBar.setDividerColor(Color.GRAY);
+
+        titleBar.setActionTextColor(Color.WHITE);
+        mCollectView = (ImageView) titleBar.addAction(new TitleBar.ImageAction(R.mipmap.collect) {
+            @Override
+            public void performAction(View view) {
+                Toast.makeText(MainActivity.this, "点击了收藏", Toast.LENGTH_SHORT).show();
+                mCollectView.setImageResource(R.mipmap.fabu);
+            }
+        });
+
+        titleBar.addAction(new TitleBar.TextAction("发布") {
+            @Override
+            public void performAction(View view) {
+                Toast.makeText(MainActivity.this, "点击了发布", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public static boolean hasKitKat() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+
+    public static boolean hasLollipop() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+}
